7804e06031a0de55d2666dd20cffcec1ce29d2a2
Harman <harmansandhu23hs@gmail.com>
2020-02-02 00:51:53 -0600

    updated layout file

--- app/src/main/java/com/greymatter/snowline/MainActivity.java
+++ app/src/main/java/com/greymatter/snowline/MainActivity.java
@@ -3,12 +3,22 @@ package com.greymatter.snowline;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText stopNumber;
+    private Button findSchedule;
+    private ListView scheduleList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stopNumber = findViewById(R.id.enter_stop_num);
+        findSchedule = findViewById(R.id.find_stop);
+        scheduleList = findViewById(R.id.schedule_list);
     }
 }
